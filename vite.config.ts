e9ec5cd5d3de77a8b95de8dc3ae6282ae1760a7a
Yuuta Ethan Tomooka <yuuta1187@kph.biglobe.ne.jp>
2026-03-12 00:38:31 -0500

    set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+	base: '/p84/',
+	plugins: [
+	react({
+		babel: {
+		plugins: [['babel-plugin-react-compiler']],
+		},
+	}),
 	tailwindcss(),
-  ],
+	],
 })
